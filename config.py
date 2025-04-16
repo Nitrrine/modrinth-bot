748e010b0eefe67f3e004b37fe959372df4f0361
Nitrrine <nitrrine@team.lodine.xyz>
2025-04-16 20:03:53 +0500

    Make forum trigger only work on original posters

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ ACTIVE_ROLE = discord.Object(id=getenv("ACTIVE_ROLE"))
 MODERATOR_ROLE = discord.Object(id=getenv("MODERATOR_ROLE"))
 COMMUNITY_SUPPORT_FORUM = discord.Object(id=getenv("COMMUNITY_SUPPORT_FORUM"))
 COMMUNITY_SUPPORT_FORUM_SOLVED_TAG = discord.Object(
-    id=getenv("COMMUNITY_SUPPORT_FORUM_SOLVED_TAG")
+  id=getenv("COMMUNITY_SUPPORT_FORUM_SOLVED_TAG")
 )
 FIND_A_PROJECT_FORUM = discord.Object(id=getenv("FIND_A_PROJECT_FORUM"))
 FIND_A_PROJECT_FORUM_SOLVED_TAG = discord.Object(
-    id=getenv("FIND_A_PROJECT_FORUM_SOLVED_TAG")
+  id=getenv("FIND_A_PROJECT_FORUM_SOLVED_TAG")
 )
 ALERT_CHANNEL = discord.Object(id=getenv("ALERT_CHANNEL"))
 GUILD = discord.Object(id=getenv("GUILD"))
